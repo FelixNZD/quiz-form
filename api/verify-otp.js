dbc847b03df80bb36e166ba73dc2761dd33197e5
FelixNZD <felix@axerevenue.com>
2025-04-23 21:09:28 +1200

    Update and rename verify-otp.js.rtf to verify-otp.js

---
+++ api/verify-otp.js
@@ -0,0 +1,18 @@
+const { verifyOtp } = require('../twilio-integration');
+
+exports.handler = async (event) => {
+  try {
+    const body = JSON.parse(event.body);
+    const result = await verifyOtp({ body });
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ success: false, error: error.message })
+    };
+  }
+};
