832fefc74222057f2b4a22e9119a7d5afe9b50de
FelixNZD <felix@axerevenue.com>
2025-04-23 21:00:01 +1200

    Update and rename send-otp.js.rtf to send-otp.js

---
+++ api/send-otp.js
@@ -0,0 +1,18 @@
+const { sendOtp } = require('../twilio-integration');
+
+exports.handler = async (event) => {
+  try {
+    const body = JSON.parse(event.body);
+    const result = await sendOtp({ body });
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ success: false, error: error.message })
+    };
+  }
+};
